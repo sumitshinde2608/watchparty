314828cb8251eb832958b32fd9be998b6c463aeb
sumitshinde2608 <shindesumit268@gmail.com>
2021-01-04 00:47:39 +0530

    Commit 1

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -8,7 +8,7 @@ const Chat = ({location}) => {
 
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
-    const ENDPOINT = 'localhost:5000';
+    const ENDPOINT = 'http://localhost:5000:/';
 
 useEffect(() => {
     const { name, room } = queryString.parse(location.search)
@@ -17,7 +17,15 @@ useEffect(() => {
         setName(name);
         setRoom(room);
 
-        console.log(socket);
+        socket.emit('join', {name , room }, () => {
+
+        });
+
+        return () => {
+            socket.emit('disconnect');
+
+            socket.off();
+        }
 },[ENDPOINT, location.search]);
 
     return (
