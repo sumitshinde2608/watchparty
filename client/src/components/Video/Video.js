c99a5f3b92f42cd7939f236ff1dfb13d505d70ad
sumitshinde2608 <shindesumit268@gmail.com>
2021-01-08 19:40:25 +0530

    Time managed

--- client/src/components/Video/Video.js
+++ client/src/components/Video/Video.js
@@ -1,23 +1,25 @@
-import React, {useState , useEffect } from 'react'; 
+import React, {useState , useEffect , useRef } from 'react'; 
 
 import ReactPlayer from 'react-player';
-// const ref = React.createRef();
-let time = '';
+
 
 const Video = () => {
+    const time = useRef(null);
+     useEffect(()=>{
+        //  time.current = time.getCurrentTime();
+        // console.log(time.current.getCurrentTime());
 
-    // useEffect(()=>{
-    //     time = ref.getCurrentTime();
-    //     console.log(time)
-
-    // },time);
+    },[]);
+    const getTime = () => {
+        console.log(time.current.getCurrentTime());
+    }
 
     return(
     
     <div className = "VideoContainer">
-        <ReactPlayer url='https://www.youtube.com/watch?v=7sDY4m8KNLc' />
-        
-        
+        <ReactPlayer url='https://www.youtube.com/watch?v=7sDY4m8KNLc' ref = {time}/>
+        {/* <div>Current time : {time.current}</div> */}
+        <button onClick= {getTime}>Click  </button>
     </div>
 
     );
