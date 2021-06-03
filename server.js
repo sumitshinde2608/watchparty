ce012f99305fc5fde5285a9e61f7840147a709fa
Nihar Sanda <nihar.sanda@gmail.com>
2021-06-03 20:54:12 +0530

    heroku deployment testing

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(router);
 
 // ... other app.use middleware 
-app.use(express.static(path.join(__dirname, "client", "build")))
+app.use(express.static('client/build'));
 
 // ...
 // Right before your app.listen(), add this:
