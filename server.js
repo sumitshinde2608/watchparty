9d38882e2ba122ec35a0e071ee9081a899a05186
Nihar Sanda <nihar.sanda@gmail.com>
2021-06-03 19:04:30 +0530

    heroku deployment testing

--- server.js
+++ server.js
@@ -17,21 +17,30 @@ app.use(cors());
 
 app.use(router);
 
+// ... other app.use middleware 
+app.use(express.static(path.join(__dirname, "client", "build")))
 
-io.on('connect', (socket) => { 
+// ...
+// Right before your app.listen(), add this:
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+
+io.on('connect', (socket) => {
   socket.on('join', ({ name, room }, callback) => {
     const { error, user } = addUser({ id: socket.id, name, room });
 
-    if(error) return callback(error);
-    const userinRoom = getUsersInRoom(user.room); 
+    if (error) return callback(error);
+    const userinRoom = getUsersInRoom(user.room);
     socket.join(user.room);
-    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.`});
+    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.` });
     socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
     // socket.emit('realtime',{user.time})
     io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
 
     console.log(userinRoom);
-    if(userinRoom.length>=2){
+    if (userinRoom.length >= 2) {
       console.log(userinRoom[0])
       io.to(userinRoom[0].id).emit("requestPlayerInfo");
     }
@@ -45,22 +54,22 @@ io.on('connect', (socket) => {
   // });
 
 
-  socket.on('sendPlayerState',(playerInfo,callback)=>{
+  socket.on('sendPlayerState', (playerInfo, callback) => {
 
     const user = getUserByID(socket.id);
     console.log(user);
-    if(user.length>=1){
-      io.in(user[0].room).emit('getPlayerInfo',playerInfo) 
+    if (user.length >= 1) {
+      io.in(user[0].room).emit('getPlayerInfo', playerInfo)
     }
-    else { 
+    else {
       callback();
     }
   })
 
-  socket.on('setPlayPause', (playerInfo, callback)=>{
+  socket.on('setPlayPause', (playerInfo, callback) => {
     const user = getUserById(socket.id);
-    if(user.length>=1){
-      io.in(user[0].room).emit('getPlayerInfo',playerInfo) 
+    if (user.length >= 1) {
+      io.in(user[0].room).emit('getPlayerInfo', playerInfo)
     }
   })
 
@@ -75,22 +84,12 @@ io.on('connect', (socket) => {
   socket.on('disconnect', () => {
     const user = removeUser(socket.id);
 
-    if(user) {
+    if (user) {
       io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
-      io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+      io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
     }
   })
 });
 
-app.use(express.static(path.join(__dirname, 'client/build')));
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
-  app.get('*', (req, res) => {
-    res.sendfile(
-      path.join(__dirname = 'client/build/index.html')
-    );
-  }
-  )
-}
 
 server.listen(process.env.PORT || 5000, () => console.log(`Server has started.`));
